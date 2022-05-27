c4c72717478dc939ca6d2455f600af8e94b1ddcc
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-05-27 20:00:35 -0300

    Pega dados do cliente do pedido

--- app/Models/PedidosCompras.php
+++ app/Models/PedidosCompras.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Clientes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class PedidosCompras extends Model
 {
     protected $fillable = ['cliente_id', 'produto_id', 'status', 'total_geral'];
+    protected $appends = ['clientes'];
     use HasFactory;
 
     public function clientes() 
@@ -18,5 +21,11 @@ class PedidosCompras extends Model
     public function produtos() 
     {
         return $this->hasOne(Produtos::class);
+    }
+
+    public function getClientesAttribute()
+    {
+        return Clientes::find(1)->pedido();
+        //return Clientes::find(4);
     }
 }
